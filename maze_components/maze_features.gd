d2b9d5e08b6ae6f42e10c282a92c7fcc81bf80b1
Dan Miller <dsmiller95@gmail.com>
2023-09-17 18:11:30 -0500

    place exit marker, and player at entry corner

---
+++ maze_components/maze_features.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var maze_data : MazeConfig
+@export var exit_feature : Node3D
+@export var entry_feature : Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if exit_feature:
+		exit_feature.global_position = to_global(MazeConfig.to_3d_from_2d(maze_data.exit))
+	if entry_feature:
+		entry_feature.global_position = to_global(MazeConfig.to_3d_from_2d(maze_data.entry))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- maze_config.gd
+++ maze_config.gd
@@ -2,7 +2,13 @@ extends Node
 class_name MazeConfig       
    
 
-@export var size := Vector2(10, 10)
+@export var size := Vector2i(10, 10)
+
+@export var entry := Vector2i(0, 0)
+@export var exit := size
+
+static func to_3d_from_2d(flat: Vector2i):
+	return Vector3(flat.x, 0, flat.y)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
