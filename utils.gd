22ad846a2ccdd14073dd61aa897613bf031d9d67
Dan Miller <dsmiller95@gmail.com>
2023-09-17 20:27:38 -0500

    proper random maze

--- utils.gd
+++ utils.gd
@@ -8,3 +8,10 @@ static func partswise(a: Vector2i, b: Vector2i, f: Callable) -> Vector2i:
 
 static var min = func(a, b): return min(a, b)
 static var max = func(a, b): return max(a, b)
+
+static func shuffle_naive(arr: Array, rng: RandomNumberGenerator):
+	for i in arr.size():
+		var swap = rng.randi_range(i, arr.size() - 1)
+		var tmp = arr[swap]
+		arr[swap] = arr[i]
+		arr[i] = tmp
--- wall_creator.gd
+++ wall_creator.gd
@@ -148,18 +148,20 @@ func randomized_dfs_walls(size: Vector2i, rng: RandomNumberGenerator):
 	var reachable := Reachability.new(size)
 	var origin := Vector2i(rng.randi_range(0, size.x - 1), rng.randi_range(0, size.y - 1))
 	
-	internal_randomize_dfs_walls(reachable, origin)
+	internal_randomize_dfs_walls(reachable, origin, rng)
 	reachable.assert_fully_reachable()
 	
 	return reachable.get_walls()
 
-func internal_randomize_dfs_walls(reach: Reachability, cell: Vector2i):
-	for i in 4:
+func internal_randomize_dfs_walls(reach: Reachability, cell: Vector2i, rng: RandomNumberGenerator):
+	var indexes = [0, 1, 2, 3]
+	Utils.shuffle_naive(indexes, rng)
+	for i in indexes:
 		var next_search = cell + neighbors[i]
 		if(!reach.in_bounds(next_search) || reach.reachable(next_search)):
 			continue
 		reach.reach_between(cell, next_search)
-		internal_randomize_dfs_walls(reach, next_search)
+		internal_randomize_dfs_walls(reach, next_search, rng)
 
 
 
